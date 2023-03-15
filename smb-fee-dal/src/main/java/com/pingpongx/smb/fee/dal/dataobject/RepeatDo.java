761a2b2acda3f585ba8b00915ebf1cda87b5d7f0
徐杨清 <xuyq@pingpongx.com>
2023-03-15 09:47:38 +0800

    feature: a billing engine

--- smb-fee-dal/src/main/java/com/pingpongx/smb/fee/dal/dataobject/RepeatDo.java
+++ smb-fee-dal/src/main/java/com/pingpongx/smb/fee/dal/dataobject/RepeatDo.java
@@ -1,5 +1,6 @@
 package com.pingpongx.smb.fee.dal.dataobject;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.pingpongx.business.dal.core.BaseDO;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +18,7 @@ import java.math.BigDecimal;
 @EqualsAndHashCode(callSuper = true)
 @Accessors(chain = true)
 @Builder
+@TableName("repeat")
 public class RepeatDo extends FeeBaseDo {
 
     private static final long serialVersionUID = 8061568762858026972L;
