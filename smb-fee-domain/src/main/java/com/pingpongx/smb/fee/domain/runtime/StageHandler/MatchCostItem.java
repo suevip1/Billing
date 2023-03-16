14813ab89299acfc535be326ff564d2659586172
徐杨清 <xuyq@pingpongx.com>
2023-03-16 15:05:35 +0800

    feature: a billing engine

--- smb-fee-domain/src/main/java/com/pingpongx/smb/fee/domain/runtime/StageHandler/MatchCostItem.java
+++ smb-fee-domain/src/main/java/com/pingpongx/smb/fee/domain/runtime/StageHandler/MatchCostItem.java
@@ -10,15 +10,19 @@ import com.pingpongx.smb.fee.domain.rules.CostItemHolder;
 import com.pingpongx.smb.fee.domain.rules.Engines;
 import com.pingpongx.smb.fee.domain.runtime.BillingContext;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MatchCostItem {
     private final Engines engines;
     private final ApplicationContext applicationContext;
@@ -28,6 +32,13 @@ public class MatchCostItem {
         BillingContext context = requestReceived.getContext();
         BillingRequest request = requestReceived.getContext().getRequest();
         Engine engine = engines.getEngine(request.getCostNodeCode());
+        if (engine == null){
+            log.warn("Cost Node : "+request.getCostNodeCode()+", has no cost item in it.");
+            context.setMatchedCostItem(new ArrayList<>());
+            MatchCompleted event = new MatchCompleted(context);
+            applicationContext.publishEvent(event);
+            return;
+        }
         MatchResult result = engine.match(BillingContext.class.getSimpleName(), context);
         List<CostItem> costItemList = result.getMatchedData().stream().map(ruleHandler -> (CostItemHolder) ruleHandler).map(costItemHolder -> costItemHolder.getCostItem()).collect(Collectors.toList());
         context.setMatchedCostItem(costItemList);
