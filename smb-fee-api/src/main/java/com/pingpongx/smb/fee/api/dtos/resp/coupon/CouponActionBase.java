8032781249562802118a4e8760cf701c2325fef6
徐杨清 <xuyq@pingpongx.com>
2023-04-19 17:23:19 +0800

    feature: a billing engine

--- smb-fee-api/src/main/java/com/pingpongx/smb/fee/api/dtos/resp/coupon/CouponActionBase.java
+++ smb-fee-api/src/main/java/com/pingpongx/smb/fee/api/dtos/resp/coupon/CouponActionBase.java
@@ -3,6 +3,8 @@ package com.pingpongx.smb.fee.api.dtos.resp.coupon;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+import java.io.Serializable;
+
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type",visible = true)
 @JsonSubTypes({
         @JsonSubTypes.Type(value = CountDecrease.class, name = "CountDecrease"),
@@ -10,6 +12,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
         @JsonSubTypes.Type(value = Gifts.class, name = "Gifts"),
         @JsonSubTypes.Type(value = ScoreIncrease.class, name = "ScoreIncrease")
 })
-public class CouponActionBase implements CouponAction{
+public class CouponActionBase implements CouponAction , Serializable {
     String couponType;
 }
