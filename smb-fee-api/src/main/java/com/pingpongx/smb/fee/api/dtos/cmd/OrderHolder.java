698802b7486b350db2ce30b45666fe391eb4cbdd
徐杨清 <xuyq@pingpongx.com>
2023-04-26 16:07:48 +0800

    feature: a billing engine

--- smb-fee-api/src/main/java/com/pingpongx/smb/fee/api/dtos/cmd/OrderHolder.java
+++ smb-fee-api/src/main/java/com/pingpongx/smb/fee/api/dtos/cmd/OrderHolder.java
@@ -2,10 +2,20 @@ package com.pingpongx.smb.fee.api.dtos.cmd;
 
 import com.pingpongx.smb.export.module.Identified;
 
+import java.math.BigDecimal;
+
 public interface OrderHolder extends Identified<String> {
     String valid();
 
     String getBizOrderId();
 
     String getBizOrderType();
+
+    String getFeePayer();
+
+    String getSourceCurrency();
+
+    String getTargetCurrency();
+
+    BigDecimal getAmount();
 }
