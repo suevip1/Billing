55e44e9edad009696b03c93bbf5359527592b3ce
徐杨清 <xuyq@pingpongx.com>
2023-06-07 10:50:00 +0800

    feature: a billing engine

--- smb-fee-api/src/main/java/com/pingpongx/smb/fee/api/dtos/expr/ExprDto.java
+++ smb-fee-api/src/main/java/com/pingpongx/smb/fee/api/dtos/expr/ExprDto.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 
 @Data
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type", visible = true)
-@JsonSubTypes({@JsonSubTypes.Type(value = FixDto.class, name = "Fix"), @JsonSubTypes.Type(value = AXpBDto.class, name = "AXpB"), @JsonSubTypes.Type(value = MaxDto.class, name = "Max"), @JsonSubTypes.Type(value = MinDto.class, name = "Min"), @JsonSubTypes.Type(value = TierDto.class, name = "Tier")})
+@JsonSubTypes({@JsonSubTypes.Type(value = FixDto.class, name = "Fix"), @JsonSubTypes.Type(value = AXpBDto.class, name = "AXpB"), @JsonSubTypes.Type(value = MaxDto.class, name = "Max"), @JsonSubTypes.Type(value = MinDto.class, name = "Min"), @JsonSubTypes.Type(value = TierDto.class, name = "Tier"), @JsonSubTypes.Type(value = SumDto.class, name = "Sum")})
 @JSONType(seeAlso = {FixDto.class, AXpBDto.class, MaxDto.class, MinDto.class, TierDto.class,SumDto.class},typeKey = "type")
 public abstract class ExprDto implements Serializable {
     public abstract String getType();
